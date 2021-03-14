bc3e2531a40b03a465fbc8bb9d0e8780924c0d3f
skarfie123 <rahulpai@rocketmail.com>
2021-03-14 22:36:25 +0000

    ramTotal

---
+++ ramTotal.cmd
@@ -0,0 +1,9 @@
+@echo off
+tasklist  /FO "CSV" /NH > ram.csv
+set ramTotal=0
+for /f "usebackq tokens=1-6 delims=,K" %%a in ("ram.csv") do (
+    set /a ramTotal=%%e%%f+ramTotal
+)
+set /a ramTotal=ramTotal/1000
+echo %ramTotal% MB
+del ram.csv
