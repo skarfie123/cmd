7992a507b7a99ff35d385c4bfe9aaa1704d276af
skarfie123 <rahulpai@rocketmail.com>
2021-03-16 23:11:21 +0000

    use readme for help

--- helpcmd.cmd
+++ helpcmd.cmd
@@ -1,11 +1,13 @@
 @echo off
 IF "%1"=="" GOTO none 
+IF "%1"==".exe" GOTO exe
    more %GITHUB%\cmd\%1.cmd
 goto:eof 
  
 :none
-   echo [92mcmd:[0m
-   dir /B %GITHUB%\cmd\*.cmd
-   echo [92mexe:[0m
+   more %GITHUB%\cmd\readme.md
+goto:eof 
+ 
+:exe
    dir /B %GITHUB%\cmd\*.exe
 goto:eof
