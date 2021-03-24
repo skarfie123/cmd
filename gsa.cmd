e29907c0396c151c3d2bf0b1b4fe2ba1fa3bb471
skarfie123 <rahulpai@rocketmail.com>
2021-03-24 23:45:48 +0000

    pause between each gs

--- gsa.cmd
+++ gsa.cmd
@@ -1,2 +1,12 @@
 @echo off
-for /f "tokens=*" %%a in ('dir /ad /b') do echo [92m%%a:[0m & cd %%a & git status & cd ..
+for /f "tokens=*" %%a in ('dir /ad /b') do call :gs %%a
+
+:gs
+    if "%1"=="" exit /b 0
+    echo [92m%1%:[0m
+    cd %1
+    git status
+    cd ..
+    pause
+    echo [1A[2K[1A
+exit /b 0
