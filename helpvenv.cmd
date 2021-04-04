320da38f98e80581878319821ae9eb08bc2c0afd
skarfie123 <rahulpai@rocketmail.com>
2021-04-04 22:11:48 +0100

    show all descriptions

--- helpvenv.cmd
+++ helpvenv.cmd
@@ -1,11 +1,20 @@
 @echo off
+setlocal ENABLEDELAYEDEXPANSION
+
 IF "%1"=="" GOTO none
 IF "%1"==".desc" GOTO desc
    type %userprofile%\venv\%1.desc
-goto:eof 
- 
+goto:eof
+
 :none
-   dir /B /AD %userprofile%\venv
+   FOR /F "tokens=*" %%V IN ('dir /B /AD %userprofile%\venv') DO (
+      if exist %userprofile%\venv\%%V.desc (
+         for /f "delims=" %%D in (%userprofile%\venv\%%V.desc) do set DESC=%%D
+      ) else (
+         set DESC=
+      )
+      echo [95m%%V[0m !DESC!
+   )
 goto:eof 
  
 :desc
