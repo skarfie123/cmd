8cc0d0a372e8a3328da853e4244164c3eeb3779d
skarfie123 <rahulpai@rocketmail.com>
2021-04-17 00:33:06 +0100

    generalise ramtotal and take output direct to for

--- ramTotal.cmd
+++ ramTotal.cmd
@@ -1,10 +1,13 @@
 @echo off
-tasklist  /FO "CSV" /NH > ram.csv
+if "%~1"=="" (
+    set command=tasklist /FO "CSV" /NH
+) else (
+    set command=tasklist /FI "IMAGENAME eq %~1" /FO "CSV" /NH
+)
 set ramTotal=0
-for /f "usebackq tokens=1-6 delims=,K" %%a in ("ram.csv") do (
+for /f "tokens=1-6 delims=,K" %%a in ('%command%') do (
     set /a ramTotal=%%e%%f+ramTotal
 )
 set /a ramTotal=ramTotal/1000
 echo %ramTotal% MB
-del ram.csv
 pause
