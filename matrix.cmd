f050547e0cc73e885a6b4db6c4ce5db4b05c87be
skarfie123 <rahulpai@rocketmail.com>
2021-04-27 16:01:21 +0100

    matrix v1

---
+++ matrix.cmd
@@ -0,0 +1,6 @@
+@echo off
+color 0a
+:top
+echo %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1% %random:~-1%
+ping -n 25 500.0.0.1>nul
+goto top
