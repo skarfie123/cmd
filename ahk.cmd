41c5c5fb6e44bbdda2003a6ed90192196de0a9d1
skarfie123 <rahulpai@rocketmail.com>
2021-04-27 15:53:35 +0100

    simplify ahk

--- ahk.cmd
+++ ahk.cmd
@@ -1,2 +1,11 @@
-@echo off 
-start %GITHUB%\AHK\main.ahk
+@echo off
+if "%~1"=="" (
+    code %GITHUB%\AHK
+    exit /b 0
+)
+
+if not exist %GITHUB%\AHK\%1.ahk (
+    echo.>%GITHUB%\AHK\%1.ahk
+    echo #Include, %1.ahk>>%GITHUB%\AHK\main.ahk
+)
+npp %GITHUB%\AHK\%1.ahk
