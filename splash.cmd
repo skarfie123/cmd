1344f8ea7c567089f86e4d6726cb40cae2005416
skarfie123 <rahulpai@rocketmail.com>
2021-06-03 10:09:36 +0100

    splash

---
+++ splash.cmd
@@ -0,0 +1,20 @@
+@echo off 
+cls
+echo           RRRRRRRRRRRRRRRRPPPPPPPPPPPPPPPP      CCCCCCCCCC  CC
+echo         RR:::::::::::::::RP:::::::::::::::PP  CCC::::::::CCC:C
+echo       RR::::RRRRRRR:::::R  P:::::PPPPPP:::::PC:::::::::::::::C
+echo      R:::::R      R::::R    P::::P     P:::::::::CCCCCCCC::::C
+echo      R:::::R      R::::R    P::::P     P::::::::C       CCCCCC
+echo      R:::::R      R::::R    P::::P     P:::::::C              
+echo       RR::::RRRRRRR::::R    P::::PPPPPP::::::::C              
+echo         RR:::::::::::::R    P::::::::::::::::::C              
+echo        R:::::RRRRRR::::R    P::::PPPPPPPPC:::::C              
+echo       R:::::R     R::::R    P::::P       C:::::C              
+echo      R:::::R      R::::R    P::::P       C:::::C              
+echo     R:::::R       R::::R    P::::P        C:::::C         CCCC
+echo    R:::::R        R::::R    P:::::PP       C:::::CCCCCCCCCC::C
+echo   R:::::R         R:::::R  P:::::::P        CC:::::::::::::::C
+echo RR::::::R         R::::::RP::::::::P          CCC::::::::::CC
+echo RRRRRRRRR         RRRRRRRRPPPPPPPPPP             CCCCCCCCCC
+echo.
+echo                       Rahul Pai Creations
