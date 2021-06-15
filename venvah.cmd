2c370bec78a82c79f14991ad56f93feb3a2048f9
skarfie123 <rahulpai@rocketmail.com>
2021-06-15 20:20:03 +0100

    try env in venvah if not specified

--- venvah.cmd
+++ venvah.cmd
@@ -1,6 +1,10 @@
 @echo off
 if "%1"=="" (
-    echo Please provide a venv name
-    exit /b 1
+    if exist env\ (
+        env\Scripts\activate.bat
+    ) else (
+        echo Please provide a venv name
+        exit /b 1
+    )
 )
 %1\Scripts\activate.bat
