88e347a1c870ff29e9f7799fdb2b43215a8b7af6
skarfie123 <rahulpai@rocketmail.com>
2021-06-19 09:37:59 +0100

    show only bad in gsa

--- gs.cmd
+++ gs.cmd
@@ -1,2 +1,2 @@
 @echo off
-git status
+git status %*
--- gsa.cmd
+++ gsa.cmd
@@ -1,10 +1,26 @@
 @echo off
+setlocal ENABLEDELAYEDEXPANSION
+
+set good=On branch mainYour branch is up to date with 'origin/main'.nothing to commit, working tree clean
+set good2=On branch masterYour branch is up to date with 'origin/master'.nothing to commit, working tree clean
+
 for /f "tokens=*" %%a in ('dir /ad /b') do call :gs %%a
 
 :gs
     if "%~1"=="" exit /b 0
-    echo [92m%1:[0m
     cd %1
+    set result=
+    FOR /F "tokens=*" %%p IN ('git status') DO set result=!result!%%p
+    IF "%good%" EQU "!result!" (
+        cd ..
+        exit /b 0
+    )
+    IF "%good2%" EQU "!result!" (
+        cd ..
+        exit /b 0
+    )
+    echo.
+    echo [92m%1:[0m
     git status 2>&1
     cd ..
     pause
